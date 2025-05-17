44b64c8de426a042e6efa866be626906a948aac2
Sam Sadr Azodi <samsadrazodi@Sams-MacBook-Pro-2.local>
2025-05-17 14:14:10 -0400

    config file updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Weather-App/', // 🔥 Must match the GitHub repo name exactly (case-sensitive!)
+ 
 })
